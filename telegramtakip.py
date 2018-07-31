209ee01b767fdb53726225ae3a9e7e2f2699aa85
abcivek <asli.b.civek@gmail.com>
2018-07-31 23:08:28 +0300

    Create telegramtakip.py

---
+++ telegramtakip.py
@@ -0,0 +1,17 @@
+import threading
+import telegram
+import os
+
+def gonder():
+    threading.Timer(20.0, gonder).start()
+    os.system("py gunluktakip.py")
+    with open("takip.txt") as f:
+        metin = f.read()
+
+    mytoken = "telegram api tokenınız"
+    mychatid = "chat id'niz"  # Chat id için: https://api.telegram.org/bot<yourtoken>/getUpdates
+    bot = telegram.Bot(token=mytoken)
+    bot.send_message(chat_id=mychatid, text=metin)
+
+
+gonder()
